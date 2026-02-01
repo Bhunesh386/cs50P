212f57c358e2fd7ecc648c66e2729332415ab3e4
bhunesh Sharma <bhuvanesh8887@gmail.com>
2026-02-01 22:48:07 +0530

    Add files via upload

---
+++ fuel.py
@@ -0,0 +1,28 @@
+def main():
+    while True:
+        try:
+            fraction = input("Fraction: ").strip()
+            x, y = fraction.split("/")
+
+            x = int(x)
+            y = int(y)
+
+            if y == 0 or x > y or x < 0:
+                raise ValueError
+
+            percentage = round((x / y) * 100)
+
+            if percentage <= 1:
+                print("E")
+            elif percentage >= 99:
+                print("F")
+            else:
+                print(f"{percentage}%")
+
+            break
+
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+main()
